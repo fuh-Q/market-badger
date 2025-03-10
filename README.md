537a6437bac851d5ad2907a358bbc952712c0f03
gdkid <77850955+fuh-Q@users.noreply.github.com>
2025-03-09 20:56:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+## Market Badge Assistant
+
+A self-hosted Discord bot that aids you in grinding Dank Memer's "Market Manipulator" badge
+
+#### Assumptions
+
+-   You are grinding the badge on a computer (and NOT on a phone)
+-   You have an alt account to post all of your market offers
+    (this makes copying the private offer IDs a lot more convenient)
+
+#### Installation
+
+1. **You will need a Rust compiler.**
+   You can install the Rust toolchain [here](https://rustup.rs/)
+
+2. **Clone this repo**
+
+    ```sh
+    git clone https://github.com/fuh-Q/market-badger
+    cd market-badger
+    ```
+
+3. **Create a bot application on Discord's Developer Portal**
+
+    Go to the [dev portal](https://discord.com/developers) and create an application. Next, you'll wanna go to the "Bot" tab, and scroll down to the bottom. Flick on the switch labeled "Message Content Intent", and make sure you save your changes!
+
+    ![bot tab](assets/bottab.png)
+    ![intents](assets/intents.png)
+
+    After that, head to the OAuth2 tab on the left. This is where you will obtain an invite link for your bot.
+
+    Select the "bot" scope from the list, and choose whatever permissions you want your bot to have. I recommend just fucking and chucking it with full Admin perms, because I made this bot under the assumption that it's being used in a private server with basically nothing in it
+
+    ![oauth2](assets/oauth2.png)
+
+    You can now copy your invite URL at the bottom, paste it in your browser, and add the bot to a server
+
+    ![copy](assets/copy-url.png)
+
+    Finally, go back the "Build-A-Bot" page in the Bot tab, where you'll wanna reset and **_COPY YOUR TOKEN!_** (this is important)
+
+    ![bot tab](assets/bottab.png)
+    ![token](assets/token.png)
+
+4. **Configure the bot**
+
+    You will notice two files in the `market-badger` directory:
+
+    - `token.txt`
+    - `codes.txt`
+
+    Firstly, you'll wanna paste your token into the `token.txt` file.
+
+    For `codes.txt`, you first need to post ALL of your market offers as **_PRIVATE_** offers, then transcribe the offer IDs into this file, separated by newlines (the bot will only read lines that begin with `PV`, the common prefix for private market offers)
+
+5. **Run the bot**
+
+    To run the bot, ensure that your command terminal is in the `market-badger` directory, and simply type
+
+    ```sh
+    cargo run --release
+    ```
+
+    Anytime you want to start the bot again, just re-run the above command
+
+#### Usage
+
+The bot has a `!ping` command, which allows you to verify that it's online.
+To start grinding, run `pls market accept` on ANY market offer, and the bot will begin cycling through the offer IDs specified in `codes.txt`. You MUST run all commands using the `pls` prefix.
+![usage](assets/copy-cmd.png)
+
+#### Questions?
+
+Ping `gdkid` in [this server](https://discord.gg/gKEKpyXeEB)
---
+++ src/main.rs
@@ -0,0 +1,180 @@
+use std::sync::Arc;
+
+use dotenv;
+
+use serenity::all::{Http, MessageUpdateEvent, ShardManager, UserId};
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+
+const DANK_MEMER: u64 = 270904126974590976;
+
+struct Owner;
+impl TypeMapKey for Owner {
+    type Value = UserId;
+}
+
+struct ClientShards;
+impl TypeMapKey for ClientShards {
+    type Value = Arc<ShardManager>;
+}
+
+struct MarketCodeFlow;
+impl TypeMapKey for MarketCodeFlow {
+    type Value = Box<dyn Iterator<Item = &'static str> + Send + Sync>;
+}
+
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content != "!ping" {
+            return;
+        }
+
+        let data = ctx.data.read().await;
+        let shard_mngr = data
+            .get::<ClientShards>()
+            .expect("expected shard manager as a key");
+
+        let runners = shard_mngr.runners.lock().await;
+        let shard = match runners.get(&ctx.shard_id) {
+            Some(runner) => runner,
+            None => {
+                if let Err(e) = msg.reply_ping(&ctx.http, "internal error").await {
+                    eprintln!("could not send msg: {e}");
+                }
+                return;
+            }
+        };
+
+        let fmt = |l: std::time::Duration| format!("`{:.2}ms`", l.as_secs_f32() / 1000f32);
+        let res = match shard.latency {
+            Some(latency) => msg.reply_ping(&ctx.http, fmt(latency)).await,
+            None => msg.reply_ping(&ctx.http, "pong").await,
+        };
+
+        if let Err(e) = res {
+            eprintln!("could not send message: {e}");
+        }
+    }
+
+    async fn message_update(
+        &self,
+        ctx: Context,
+        old: Option<Message>,
+        _new: Option<Message>,
+        _: MessageUpdateEvent,
+    ) {
+        let msg = match old {
+            Some(msg) => msg,
+            // if it's none, then the message cache is broken
+            None => return,
+        };
+
+        // dank memer sends all messages using embeds
+        // we can assume that msg.embeds[0] exists
+        let t = msg.embeds[0].title.as_ref();
+        if msg.author.id != DANK_MEMER
+            || t.is_none()
+            || t.is_some_and(|s| s != "Pending Confirmation")
+        {
+            return;
+        }
+
+        let invocation = match msg.referenced_message {
+            Some(refmsg) => refmsg,
+            None => return,
+        };
+
+        if !invocation
+            .content
+            .to_lowercase()
+            .starts_with("pls market accept")
+        {
+            return;
+        }
+
+        {
+            let data = ctx.data.read().await;
+            let owner = data.get::<Owner>().expect("owner's user ID not found");
+
+            if &invocation.author.id != owner {
+                return;
+            }
+        }
+
+        let code = {
+            let mut data = ctx.data.write().await;
+            let flow = data
+                .get_mut::<MarketCodeFlow>()
+                .expect("flow iterator not found");
+
+            flow.next().expect("market code flow was empty...?")
+        };
+
+        let content = format!("```\npls market accept {code} 1\n```");
+        let res = invocation.reply_ping(&ctx, content).await;
+        if let Err(e) = res {
+            eprintln!("failed to send next flow code: {e}");
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} ({}) connected", ready.user.name, ready.user.id);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let token = include_str!("../token.txt");
+    let intents = GatewayIntents::default() | GatewayIntents::MESSAGE_CONTENT;
+
+    let codes = include_str!("../codes.txt").trim();
+    if codes.is_empty() {
+        panic!("no market codes were given");
+    }
+
+    let codes_iter = codes.split_whitespace().filter(|l| l.starts_with("PV"));
+    println!("Cycling over {} offer codes...", codes_iter.clone().count());
+
+    let http = Http::new(&token);
+    let owner = match http.get_current_application_info().await {
+        Ok(info) => {
+            if let Some(team) = info.team {
+                team.owner_user_id
+            } else if let Some(owner) = &info.owner {
+                owner.id
+            } else {
+                panic!("could not determine bot owner")
+            }
+        }
+        Err(e) => panic!("could not fetch app info: {e}"),
+    };
+
+    let mut cache_config = serenity::cache::Settings::default();
+    cache_config.max_messages = 100;
+
+    let mut client = match Client::builder(&token, intents)
+        .event_handler(Handler)
+        .cache_settings(cache_config)
+        .type_map_insert::<Owner>(owner)
+        .type_map_insert::<MarketCodeFlow>(Box::new(codes_iter.cycle()))
+        .await
+    {
+        Ok(c) => c,
+        Err(e) => panic!("failed creating client: {e}"),
+    };
+
+    {
+        let mut data = client.data.write().await;
+        data.insert::<ClientShards>(Arc::clone(&client.shard_manager));
+    }
+
+    if let Err(e) = client.start().await {
+        panic!("client could not start: {e}");
+    }
+}
